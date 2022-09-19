864f852149e423bc19cb16f268eeafedf0c30c64
angie <angheloalf95@gmail.com>
2022-09-19 14:01:58 -0300

    read endianess from elf file

--- segtypes/common/elf.py
+++ segtypes/common/elf.py
@@ -1,5 +1,18 @@
+import spimdisasm
+
 from segtypes.common.code import CommonSegCode
+from util import options
 
 # elf group
 class CommonSegElf(CommonSegCode):
-    pass
+    def scan(self, rom_bytes):
+        self.elf_file = spimdisasm.elf32.Elf32File(rom_bytes)
+
+        if self.elf_file.header.ident.getDataEncoding() == spimdisasm.elf32.Elf32HeaderIdentifier.DataEncoding.DATA2MSB:
+            options.opts["endianess"] = "big"
+        else:
+            options.opts["endianess"] = "little"
+
+        # TODO: add symbols from elf
+
+        super().scan(rom_bytes)
